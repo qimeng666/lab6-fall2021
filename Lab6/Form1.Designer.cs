b17159498151e905eaff9b7bab47fc46cd4563cf
Qimeng Chao <u1297640@umail.utah.edu>
2021-09-29 19:29:39 -0600

    Add project files.

---
+++ Lab6/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿
+namespace Lab6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.text1 = new System.Windows.Forms.TextBox();
+            this.text2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.text3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // text1
+            // 
+            this.text1.Location = new System.Drawing.Point(438, 130);
+            this.text1.Name = "text1";
+            this.text1.Size = new System.Drawing.Size(150, 22);
+            this.text1.TabIndex = 0;
+            this.text1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // text2
+            // 
+            this.text2.Location = new System.Drawing.Point(438, 203);
+            this.text2.Name = "text2";
+            this.text2.Size = new System.Drawing.Size(100, 22);
+            this.text2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mistral", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+            this.label1.Location = new System.Drawing.Point(161, 130);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(107, 35);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tip :20%";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Gabriola", 28.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(58, 230);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(283, 92);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Compute Tip";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // text3
+            // 
+            this.text3.Location = new System.Drawing.Point(438, 300);
+            this.text3.Name = "text3";
+            this.text3.Size = new System.Drawing.Size(100, 22);
+            this.text3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(827, 450);
+            this.Controls.Add(this.text3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.text2);
+            this.Controls.Add(this.text1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox text1;
+        private System.Windows.Forms.TextBox text2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox text3;
+    }
+}
+
---
+++ Lab6/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab6
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            button1.Enabled = false;
+            text2.Enabled = false;
+            text3.Enabled = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           if (double.TryParse(text1.Text,out double result))
+            {
+                button1.Enabled = true;
+            }
+           if(double.TryParse (text1.Text,out double result2))
+            {
+                double totalBill = double.Parse(text1.Text) * 0.2;
+                text2.Text = "" + totalBill.ToString();
+                text3.Text = "" + (double.Parse(text1.Text) + totalBill).ToString();
+            }
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            button1.Enabled = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
